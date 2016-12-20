2bb130f302547970e728e143973ac047abaa5c07
gafuchs <gafuchs@gmail.com>
2016-12-20 20:58:41 -0200

    Addition of social medias references;

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -4,11 +4,20 @@ import Footer from "../components/Footer";
 export default class Home extends React.Component {
   render() {
     const googleProfilePic = "img/PerfilGabrielFuchs.jpg";
+    const gitHubIcon = "img/icon_github-sign.svg";
+    const linkedinIcon = "img/icon_linkedin-sign.svg";
+    const googlePlusIcon = "img/icon_googleplus-sign.svg";
+    const facebookIcon = "img/icon_facebook-sign.svg";
+
     const divStyle = {
       marginTop: "30px",
     };
     const imgStyle = {
       maxHeight: "150px",
+    };
+    const imgIconStyle = {
+      maxHeight: "30px",
+      margin: "10px",
     };
 
     return (
@@ -17,6 +26,24 @@ export default class Home extends React.Component {
          class="img-responsive img-circle center-block"
          style={imgStyle}/>
         <h1>Gabriel Amaral Fuchs</h1>
+        <h4>contato: gafuchs@gmail.com</h4>
+
+        <a href="https://github.com/GAFuchs">
+          <img src={gitHubIcon} style={imgIconStyle}/>
+        </a>
+
+        <a href="https://www.linkedin.com/in/gabriel-amaral-fuchs-b97a89122?trk=hp-identity-name">
+          <img src={linkedinIcon} style={imgIconStyle}/>
+        </a>
+
+        <a href="https://plus.google.com/u/0/111515541985509247632">
+          <img src={googlePlusIcon} style={imgIconStyle}/>
+        </a>
+
+        <a href="https://www.facebook.com/gabriel.fuchs.54?ref=br_rs">
+          <img src={facebookIcon} style={imgIconStyle}/>
+        </a>
+
         <Footer />
       </div>
     );
